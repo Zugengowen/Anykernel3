d5f5cc2ee6dab5ceb1823c4249fa75784a62b51c
dracarys18 <karthihegde010@gmail.com>
2020-06-04 11:34:53 +0500

    AK3: Force tripple frame buffer
    Based on Kerneltoast's commit this resulted in smooth ui with low jitter so let's set this through AnyKernel for all NotKernel users
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -30,7 +30,7 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
+set_perm 0 0 750 $ramdisk/init.notkernel.sh;
 ## AnyKernel install
 dump_boot;
 
---
+++ ramdisk/init.notkernel.sh
@@ -0,0 +1,3 @@
+# Force tripple frame buffer for surfaceflinger
+
+setprop ro.surface_flinger.max_frame_buffer_acquired_buffers 3
